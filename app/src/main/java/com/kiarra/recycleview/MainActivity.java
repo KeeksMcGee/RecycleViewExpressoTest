218747b19dcfe006a96d4ab7dc8629990b845805
Kiarra Gibbs <liveloveshop91@gmail.com>
2020-02-16 15:46:11 -0500

    Initial commit

---
+++ app/src/main/java/com/kiarra/recycleview/MainActivity.java
@@ -0,0 +1,87 @@
+package com.kiarra.recycleview;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+
+        //Put the initial data into the word list.
+        for (int i=0; i < 20; i++){
+            mWordList.addLast("Word " + i);
+        }
+
+        //Get a handle on the RecyclerView
+        mRecyclerView = findViewById(R.id.recyclerView);
+        //Create an adapter and supply the data to be displayed
+        mAdapter = new WordListAdapter(this, mWordList);
+        //Connect the adapter with the RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+        //Give the RecyclerView a default layout manager
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, gridColumnCount));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              int wordListSize = mWordList.size();
+              //Add new word to the wordList.
+                mWordList.addLast("+ Word " + wordListSize);
+                //Notify the adapter that the data has changed.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                //Scroll to the bottom
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
